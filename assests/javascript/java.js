ba2830a905e6b91629d41432647eb8eb26c895a3
Broc <broc.davis@yahoo.com>
2019-10-13 20:15:19 -0500

    added attributes to the displayed gifs. Fixed typo on a topic in intial array. Tested and confirmed still to animate and back to still for the gif settings.

--- assests/javascript/java.js
+++ assests/javascript/java.js
@@ -1,4 +1,4 @@
-var topics = ["destiny2", "wakboarding"];
+var topics = ["destiny2", "wakeboarding", "dominoes","explosions"];
 //dipslay topics array as buttons to click
 displayButtons();
 
@@ -21,33 +21,39 @@ $("button").on("click", function () {
         var topicDiv = $("<div>");
         var p = $("<p>").text("Rating: " + results[i].rating);
         var topicImage = $("<img>");
-        
-        topicImage.attr("src", results[i].images.fixed_height.url);
-        topicImage.attr("data-state","still");
-        
+        //grabs the still of the gif first
+        topicImage.attr("src", results[i].images.fixed_height_still.url);
+        topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+        topicImage.attr("data-animate", results[i].images.fixed_height.url);
+        topicImage.attr("data-state", "still");
+        topicImage.addClass("gif");
+
         topicDiv.append(p);
         topicDiv.append(topicImage);
 
         $("#gifs-appear-here").prepend(topicDiv);
       }
     });
-    //if gif is clicked set to animate and set to still if animated
-    $(".gif").on("click", function() {
-      var state = $(this).attr("data-state");
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-    });
+});
+
+//if a gif is clicked set to animate and set to still if animated
+$("#gifs-appear-here").on("click", ".gif", function() {
+  console.log("clicked");
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  }
 });
 //function to display each index of the array as a button
-function displayButtons(){
-  for(var i = 0; i< topics.length; i++){
-    var b = $("<button>").text(topics[i]).attr();
-    
+function displayButtons() {
+  for (var i = 0; i < topics.length; i++) {
+    var b = $("<button>").text(topics[i]);
+    b.attr({ "data-state": "still", "data-topic": topics[i] });
+
     $("#button-layout").append(b);
 
   }
